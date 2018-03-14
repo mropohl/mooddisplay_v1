916ee556f1aedb20a645bd00c98a6ebdb4a21ddf
Max Ropohl <m.ropohl@outlook.de>
2018-03-14 14:15:50 +0100

    added first try to integrate x,y boundaries based on mood level

--- dist/assets/js/main.min.js
+++ dist/assets/js/main.min.js
@@ -134,20 +134,15 @@ function draw() {
         const isAnimating = particle.animate(mX, mY, maxDist);
 
         if (!isAnimating) {
+            const maxMoveDist = 200;
+
             const distX = particle.mood * width / 2;
             const distY = particle.mood * height / 2;
 
-            const boundMax = rad * particle.mood;
-            const boundMin = rad * (particle.mood - 0.1);
-
-            const newX = Math.floor(
-                random(width / 2 - distX, width / 2 + distX)
-            );
-            const newY = Math.floor(
-                random(height / 2 - distY, height / 2 + distY)
-            );
-
-            particle.updateTargets(newX, newY);
+            let { x, y } = calculateTargetCords(particle.mood, width, height);
+            x = Math.round(x);
+            y = Math.round(y);
+            particle.updateTargets(x, y);
         }
     });
 
@@ -164,6 +159,66 @@ function draw() {
     stroke(0);
     text("FPS: " + fps.toFixed(2), 10, height - 10);
 }
+
+const calculateTargetCords = (mood, width, height) => {
+    const { x, y } = calculateBounds(mood, width, height);
+    const target = {};
+
+    target.x = random(width);
+    target.y = random(height);
+
+    while (
+        !(target.x > x.negBoundMin && target.x < x.negBoundMax) &&
+        !(target.x < x.posBoundMax && target.x > x.posBoundMin)
+    ) {
+        target.x = random(width);
+    }
+
+    while (
+        !(target.y > y.negBoundMin && target.y < y.negBoundMax) &&
+        !(target.y < y.posBoundMax && target.y > y.posBoundMin)
+    ) {
+        target.y = random(width);
+    }
+
+    return target;
+};
+
+const calculateBounds = (mood, width, height) => {
+    const bounds = { x: {}, y: {} };
+
+    bounds.x.negBoundMax = mood * width / 2;
+    bounds.x.negBoundMin = bounds.x.negBoundMax - bounds.x.negBoundMax * 0.5;
+
+    bounds.y.negBoundMin = mood * height / 2;
+    bounds.y.negBoundMax = bounds.y.negBoundMin + bounds.y.negBoundMin * 0.5;
+
+    bounds.x.posBoundMin = width / 2 + bounds.x.negBoundMax;
+    bounds.x.posBoundMax = bounds.x.posBoundMin + bounds.x.posBoundMin * 0.5;
+
+    bounds.y.posBoundMin = height / 2 + bounds.y.negBoundMax;
+    bounds.y.posBoundMax = bounds.y.posBoundMin + bounds.y.posBoundMin * 0.5;
+
+    Object.keys(bounds.x).forEach(key => {
+        if (bounds.x[key] < 0) {
+            bounds.x[key] = 0;
+        }
+        if (bounds.x[key] > width) {
+            bounds.x[key] = width;
+        }
+    });
+
+    Object.keys(bounds.y).forEach(key => {
+        if (bounds.y[key] < 0) {
+            bounds.y[key] = 0;
+        }
+        if (bounds.y[key] > height) {
+            bounds.y[key] = height;
+        }
+    });
+
+    return bounds;
+};
 
 class Particle {
     constructor(x, y, mood, r, g, b) {
